cdb090569e8eea5d12936a877a21f0f465916cb2
palomasoltys <pfs601@gmail.com>
2022-11-29 11:27:48 -0500

    Part 3 - Step 3: Create User and Note Controllers

--- src/main/java/com/devmountain/noteApp/repositories/INoteRepository.java
+++ src/main/java/com/devmountain/noteApp/repositories/INoteRepository.java
@@ -1,9 +1,14 @@
 package com.devmountain.noteApp.repositories;
 
 import com.devmountain.noteApp.entities.Note;
+import com.devmountain.noteApp.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface INoteRepository extends JpaRepository<Note, Long> {
+    List<Note> findAllByUserEquals(User user);
+
 }
